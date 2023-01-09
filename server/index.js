2cfe9ec2125ea9c42d5819d037bbe4d2fecc15a9
Monique Saviczki <monique_saviczki@hotmail.com>
2023-01-09 16:40:02 -0400

    script nodemon

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+//const mysql = require('mysql');
 
-const db = mysql.createPool({
+/* const db = mysql.createPool({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "",
     database: "crudmedcloud"
-});
+}); */
 
 app.listen(3001, () =>{
     console.log("rodando servidor");
